63cb56f0f7e56cdc19f0fa1bc210f8e60559d263
Sergio E. Prellezo <sereprec@gmail.com>
2021-06-04 16:05:54 -0300

    Limpieza inicial de la app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
+//import "./App.scss";
+import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <Navbar bg="dark" variant="dark" className="pt-3">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+
         <p>Hola Mundo!</p>
         <a
           className="App-link"
