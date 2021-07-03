4440e1f0b4359ac5fd076df8c0e5eabd368955c1
Sergio E. Prellezo <sereprec@gmail.com>
2021-07-03 20:08:03 -0300

    Centralizando lógica de carga del carrito guardado el localstorage en Cart

--- src/components/EmptyCart/index.jsx
+++ src/components/EmptyCart/index.jsx
@@ -1,84 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Loader from "../Loader";
 import TypicButton from "../TypicButton";
-import { getProducts } from "../../utils/getProducts";
-import { modalMessages } from "../../utils/cartModalMessages";
 import iconCart from "../../assets/img/icon_cart2_red.png";
 import "./EmptyCart.scss";
 
-const EmptyCart = ({ setCart, handleShowModal, setContentModal }) => {
-  const [savedCart, setSavedCart] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  //const [isError, setIsError] = useState(false);
-
-  const getSavedCart = () => {
-    const localCart =
-      localStorage.getItem("myMammothSavedCart") &&
-      JSON.parse(localStorage.getItem("myMammothSavedCart"));
-    //verifico que exista y sea de un formato válido
-    if (
-      localCart &&
-      localCart.length > 0 &&
-      localCart[0].product &&
-      localCart[0].qty
-    ) {
-      setSavedCart(localCart);
-      return true;
-    } else {
-      setSavedCart(null);
-      return false;
-    }
-  };
-
-  const loadSavedCart = () => {
-    setIsLoading(true);
-    let modalMsg;
-    const dataSavedCart = savedCart.map(elem => {
-      return { id: elem.product.id, qty: elem.qty };
-    });
-    return getProducts()
-      .then(res => {
-        const checkedCart = [];
-        res.forEach(product => {
-          for (const data of dataSavedCart) {
-            if (product.id === data.id && product.stock >= data.qty) {
-              const qty = data.qty;
-              checkedCart.push({ product, qty });
-            }
-          }
-        });
-        setCart(checkedCart);
-        localStorage.removeItem("myMammothSavedCart");
-        if (savedCart.length === checkedCart.length) {
-          modalMsg = modalMessages[1];
-        } else if (checkedCart.length === 0) {
-          modalMsg = modalMessages[3];
-          setSavedCart(null);
-          setIsLoading(false);
-        } else {
-          modalMsg = modalMessages[2];
-        }
-        setContentModal(modalMsg);
-      })
-      .catch(err => {
-        setCart(null);
-        setContentModal(modalMessages[4]);
-        //setIsError(err);
-        //setIsLoading(false);
-      })
-      .finally(() => {
-        handleShowModal();
-      });
-  };
-
-  const handleLoad = () => {
-    loadSavedCart();
-  };
-
+const EmptyCart = ({ getSavedCart, savedCart, loadSavedCart, isLoading }) => {
   useEffect(() => {
     getSavedCart();
-  }, []);
+  }, [getSavedCart]);
 
   if (isLoading)
     return (
@@ -95,7 +25,7 @@ const EmptyCart = ({ setCart, handleShowModal, setContentModal }) => {
       {savedCart && (
         <div>
           <p>Encontramos un carrito guardado...</p>
-          <TypicButton className="font-weight-bold" onClick={handleLoad}>
+          <TypicButton className="font-weight-bold" onClick={loadSavedCart}>
             Cargar Carrito
           </TypicButton>
         </div>
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,15 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useCallback } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { CartContext } from "../../context/CartContext";
 import CartDetail from "../../components/CartDetail";
 import EmptyCart from "../../components/EmptyCart";
 import InfoBar from "../../components/InfoBar";
+import { getProducts } from "../../utils/getProducts";
 import { modalMessages } from "../../utils/cartModalMessages";
 import iconCart from "../../assets/img/icon_cart2.png";
 import "./Cart.scss";
 
 const Cart = () => {
   const cartContext = useContext(CartContext);
+
+  const [savedCart, setSavedCart] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  //const [isError, setIsError] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [contentModal, setContentModal] = useState(modalMessages[0]);
 
@@ -28,6 +33,67 @@ const Cart = () => {
     setContentModal(modalMessages[5]);
   };
 
+  const getSavedCart = useCallback(() => {
+    const localCart =
+      localStorage.getItem("myMammothSavedCart") &&
+      JSON.parse(localStorage.getItem("myMammothSavedCart"));
+    //verifico que exista y sea de un formato válido
+    if (
+      localCart &&
+      localCart.length > 0 &&
+      localCart[0].product &&
+      localCart[0].qty
+    ) {
+      setSavedCart(localCart);
+      return true;
+    } else {
+      setSavedCart(null);
+      return false;
+    }
+  }, []);
+
+  const loadSavedCart = () => {
+    setIsLoading(true);
+    let modalMsg;
+    const dataSavedCart = savedCart.map(elem => {
+      return { id: elem.product.id, qty: elem.qty };
+    });
+    return getProducts()
+      .then(res => {
+        const checkedCart = [];
+        res.forEach(product => {
+          for (const data of dataSavedCart) {
+            if (product.id === data.id && product.stock >= data.qty) {
+              const qty = data.qty;
+              checkedCart.push({ product, qty });
+            }
+          }
+        });
+        setCart(checkedCart);
+        localStorage.removeItem("myMammothSavedCart");
+        if (savedCart.length === checkedCart.length) {
+          modalMsg = modalMessages[1];
+        } else if (checkedCart.length === 0) {
+          modalMsg = modalMessages[3];
+        } else {
+          modalMsg = modalMessages[2];
+        }
+        setContentModal(modalMsg);
+      })
+      .catch(err => {
+        setCart(null);
+        setContentModal(modalMessages[4]);
+        //setIsError(err);
+        //setIsLoading(false);
+      })
+      .finally(() => {
+        handleShowModal();
+        setIsLoading(false);
+        setSavedCart(null);
+      });
+  };
+
+  const emptyCartProps = { getSavedCart, savedCart, loadSavedCart, isLoading };
   const cartDetailProps = { ...cartContext, handleSaveCart };
 
   return (
@@ -39,11 +105,7 @@ const Cart = () => {
         {cart.length > 0 ? (
           <CartDetail {...cartDetailProps} />
         ) : (
-          <EmptyCart
-            setCart={setCart}
-            handleShowModal={handleShowModal}
-            setContentModal={setContentModal}
-          />
+          <EmptyCart {...emptyCartProps} />
         )}
       </div>
       <Modal
