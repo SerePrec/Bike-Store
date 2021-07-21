2f6276da8e426ce9065d76848811f30140a5c9d2
Sergio E. Prellezo <sereprec@gmail.com>
2021-07-21 11:12:19 -0300

    React.memo en tarjetas. Componente Item

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -7,39 +7,42 @@ import regularHeart from "../../assets/img/heart-regular.svg";
 import solidHeart from "../../assets/img/heart-solid.svg";
 import "./Item.scss";
 
-const Item = ({ product, isFav }) => {
-  const { id, title, brand, price, discount, pictureURL, stock } = product;
-  let history = useHistory();
+const Item = React.memo(
+  ({ product, isFav }) => {
+    const { id, title, brand, price, discount, pictureURL, stock } = product;
+    let history = useHistory();
 
-  const goDetail = id => {
-    history.push(`/item/${id}`);
-  };
+    const goDetail = id => {
+      history.push(`/item/${id}`);
+    };
 
-  return (
-    <Card
-      className={`h-100 shadow ${stock < 1 ? "card--noStock" : ""}`}
-      onClick={stock > 0 ? () => goDetail(id) : null}
-    >
-      <div className="fav">
-        <img src={isFav ? solidHeart : regularHeart} alt="" />
-      </div>
-      {discount !== 0 && <div className="discount">{discount}%</div>}
-      <ImgWidthLoader
-        className="card-img-top"
-        pictureURL={pictureURL}
-        alt={title}
-      />
-      <Card.Body>
-        <Card.Title>{brand}</Card.Title>
-        <Card.Text>{title}</Card.Text>
-        <Card.Text className="mt-2">
-          <b>${priceFormat(price * (1 - discount / 100))}</b>
-          {discount !== 0 && <del>${priceFormat(price)}</del>}
-        </Card.Text>
-      </Card.Body>
-      {stock < 1 && <div className="text--noStock">AGOTADO</div>}
-    </Card>
-  );
-};
+    return (
+      <Card
+        className={`h-100 shadow ${stock < 1 ? "card--noStock" : ""}`}
+        onClick={stock > 0 ? () => goDetail(id) : null}
+      >
+        <div className="fav">
+          <img src={isFav ? solidHeart : regularHeart} alt="" />
+        </div>
+        {discount !== 0 && <div className="discount">{discount}%</div>}
+        <ImgWidthLoader
+          className="card-img-top"
+          pictureURL={pictureURL}
+          alt={title}
+        />
+        <Card.Body>
+          <Card.Title>{brand}</Card.Title>
+          <Card.Text>{title}</Card.Text>
+          <Card.Text className="mt-2">
+            <b>${priceFormat(price * (1 - discount / 100))}</b>
+            {discount !== 0 && <del>${priceFormat(price)}</del>}
+          </Card.Text>
+        </Card.Body>
+        {stock < 1 && <div className="text--noStock">AGOTADO</div>}
+      </Card>
+    );
+  },
+  (prevProps, nextProps) => prevProps.isFav === nextProps.isFav
+);
 
 export default Item;
